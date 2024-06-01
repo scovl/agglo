1fda0316449c322480e7df97c864e429d68bde68
Vitor Lobo Ramos <lobocode@gmail.com>
2024-06-01 18:55:54 -0300

    Refactor handler.clj to apply the SRP by adding auxiliary functions for HTML loading, placeholder replacement, and content rendering.

--- src/clj/agglo/handler.clj
+++ src/clj/agglo/handler.clj
@@ -1,34 +1,53 @@
 (ns agglo.handler
   (:require [ring.util.response :as response]
             [agglo.feed :as feed]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
 
-(defn home-page []
-  (let [html-file (io/resource "public/index.html")
-        feeds (feed/fetch-feeds)]
-    (if html-file
-      (-> (slurp html-file)
-          (str/replace "{{feeds}}"
-                       (apply str
-                              (map (fn [{:keys [title link description]}]
-                                     (format "<div class='feed'>
-                                               <h2><a href='%s'>%s</a></h2>
-                                               <p>%s</p>
-                                              </div>"
-                                             link title (subs description 0 (min 00 (count description)))))
-                                   feeds)))
-          (response/response)
-          (response/content-type "text/html"))
-      (response/status (response/response "HTML file not found") 800))))
+(defn load-html-file [resource-path]
+  (let [resource (io/resource resource-path)]
+    (if resource
+      (slurp resource)
+      (do
+        (println "HTML file not found at" resource-path)
+        nil))))
 
-(defn feeds []
+(defn replace-placeholder [html placeholder content]
+  (str/replace html placeholder content))
+
+(defn render-home-page [feeds]
+  (let [html-content (load-html-file "public/index.html")]
+    (if html-content
+      (replace-placeholder html-content
+                           "{{feeds}}"
+                           (apply str
+                                  (map (fn [{:keys [title link description]}]
+                                         (format "<div class='feed'>
+                                                   <h2><a href='%s'>%s</a></h2>
+                                                   <p>%s</p>
+                                                  </div>"
+                                                 link title (first (str/split description #"\n\n"))))
+                                       feeds)))
+      "HTML file not found")))
+
+(defn home-page-handler [request]
+  (let [feeds (feed/fetch-feeds)
+        rendered-content (render-home-page feeds)]
+    (if (= rendered-content "HTML file not found")
+      (-> (response/response rendered-content)
+          (response/status 500)
+          (response/content-type "application/json"))
+      (-> (response/response rendered-content)
+          (response/content-type "text/html; charset=utf-8")))))
+
+(defn feeds-handler [request]
   (response/response (feed/fetch-feeds)))
 
-(defn blog-links []
+(defn blog-links-handler [request]
   (response/response (feed/load-config)))
 
 (def app
   (ring/ring-handler
    (ring/router
-    [["/" {:get home-page}]
-     ["/blog-links" {:get blog-links}]])))
+    [["/" {:get home-page-handler}]
+     ["/blog-links" {:get blog-links-handler}]])))
