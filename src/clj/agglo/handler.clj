c07874f958580bf674ba04f48b23e6aefc438289
Vitor Lobo Ramos <lobocode@gmail.com>
2024-06-01 15:53:04 -0300

    define home-page feeds into index

--- src/clj/agglo/handler.clj
+++ src/clj/agglo/handler.clj
@@ -4,8 +4,22 @@
             [clojure.java.io :as io]))
 
 (defn home-page []
-  (response/content-type
-   (response/resource-response "public/index.html") "text/html"))
+  (let [html-file (io/resource "public/index.html")
+        feeds (feed/fetch-feeds)]
+    (if html-file
+      (-> (slurp html-file)
+          (str/replace "{{feeds}}"
+                       (apply str
+                              (map (fn [{:keys [title link description]}]
+                                     (format "<div class='feed'>
+                                               <h2><a href='%s'>%s</a></h2>
+                                               <p>%s</p>
+                                              </div>"
+                                             link title (subs description 0 (min 00 (count description)))))
+                                   feeds)))
+          (response/response)
+          (response/content-type "text/html"))
+      (response/status (response/response "HTML file not found") 800))))
 
 (defn feeds []
   (response/response (feed/fetch-feeds)))
@@ -17,5 +31,4 @@
   (ring/ring-handler
    (ring/router
     [["/" {:get home-page}]
-     ["/feeds" {:get feeds}]
      ["/blog-links" {:get blog-links}]])))
