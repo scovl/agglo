ffd71ed3e0f53e92364b92795b909732284adfeb
Vitor Lobo Ramos <lobocode@gmail.com>
2024-06-03 13:04:39 -0300

    Remove duplicate render-home-page function

--- src/clj/agglo/handler.clj
+++ src/clj/agglo/handler.clj
@@ -1,42 +1,18 @@
 (ns agglo.handler
   (:require [ring.util.response :as response]
             [agglo.feed :as feed]
-            [clojure.java.io :as io]
-            [clojure.string :as str]
-            [clojure.tools.logging :as log]))
-
-(defn render-home-page [html-content feeds]
-  (try
-    (-> html-content
-        (str/replace "{{feeds}}"
-                     (apply str
-                            (map (fn [{:keys [title link description]}]
-                                   (let [description-text (if (map? description)
-                                                            (:value description)
-                                                            description)]
-                                     (format "<div class='feed'>
-                                               <h2><a href='%s'>%s</a></h2>
-                                               <p>%s</p>
-                                              </div>"
-                                             link title (if (string? description-text)
-                                                          (first (str/split description-text #"\n"))
-                                                          description-text))))
-                                 feeds))))
-    (catch Exception e
-      (log/error e "Error rendering home page")
-      "Internal server error")))
+            [agglo.service :refer [render-home-page]]
+            [clojure.java.io :as io]))
 
 (defn home-page []
-  (let [html-file-path "resources/public/index.html"
-        html-file (io/file html-file-path)
+  (let [html-file (io/resource "public/index.html")
         feeds (feed/fetch-feeds)]
-    (if (.exists html-file)
-      (-> (render-home-page (slurp html-file) feeds)
+    (if html-file
+      (-> (slurp html-file)
+          (render-home-page feeds)
           (response/response)
-          (response/content-type "text/html; charset=utf-8"))
-      (do
-        (log/error "HTML file not found at" html-file-path)
-        (response/status (response/response "HTML file not found") 500)))))
+          (response/content-type "text/html"))
+      (response/status (response/response "HTML file not found") 500))))
 
 (defn feeds []
   (response/response (feed/fetch-feeds)))
