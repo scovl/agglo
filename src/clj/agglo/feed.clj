2bd16280fb112f67e1d11e4549cc32156944a1d1
Vitor Lobo Ramos <lobocode@gmail.com>
2024-06-02 21:28:25 -0300

    add support for parsing both RSS and ATOM feeds

--- src/clj/agglo/feed.clj
+++ src/clj/agglo/feed.clj
@@ -1,17 +1,108 @@
 (ns agglo.feed
-  (:require [clj-http.client :as client]
-            [clojure.xml :as xml]
-            [clojure.zip :as zip]
-            [clojure.data.zip.xml :as zf]
+  (:import [com.sun.syndication.io SyndFeedInput XmlReader]
+           [java.net URL]
+           [java.io ByteArrayInputStream])
+  (:require [clojure.java.io :as io]
+            [clojure.tools.logging :as log]
             [clojure.edn :as edn]
-            [clojure.java.io :as io]
-            [clojure.tools.logging :as log])
-  (:import (java.io ByteArrayInputStream)))
+            [clj-http.client :as client]))
 
-(defn safe-get [ctx path-fn default]
-  (try
-    (path-fn ctx)
-    (catch IllegalArgumentException _ default)))
+(defrecord feed [authors author categories contributors copyright description
+                 encoding entries feed-type image language link entry-links
+                 published-date title uri])
+
+(defrecord entry [authors author categories contents contributors description
+                  enclosures link published-date title updated-date url])
+
+(defrecord enclosure [length type uri])
+(defrecord person    [email name uri])
+(defrecord category  [name taxonomy-uri])
+(defrecord content   [type value])
+(defrecord image     [description link title url])
+(defrecord link      [href hreflang length rel title type])
+
+(defn- obj->enclosure [e]
+  (map->enclosure {:length (.getLength e)
+                   :type   (.getType e)
+                   :url    (.getUrl e)}))
+
+(defn- obj->content [c]
+  (map->content {:type  (.getType c)
+                 :value (.getValue c)}))
+
+(defn- obj->link [l]
+  (map->link {:href     (.getHref l)
+              :hreflang (.getHreflang l)
+              :length   (.getLength l)
+              :rel      (.getRel l)
+              :title    (.getTitle l)
+              :type     (.getType l)}))
+
+(defn- obj->category [c]
+  (map->category {:name         (.getName c)
+                  :taxonomy-uri (.getTaxonomyUri c)}))
+
+(defn- obj->person [sp]
+  (map->person {:email (.getEmail sp)
+                :name  (.getName sp)
+                :uri   (.getUri sp)}))
+
+(defn- obj->image [i]
+  (map->image {:description (.getDescription i)
+               :link        (.getLink i)
+               :title       (.getTitle i)
+               :url         (.getUrl i)}))
+
+(defn- obj->entry [e]
+  (map->entry {:authors        (map obj->person    (seq (.getAuthors e)))
+               :categories     (map obj->category  (seq (.getCategories e)))
+               :contents       (map obj->content   (seq (.getContents e)))
+               :contributors   (map obj->person    (seq (.getContributors e)))
+               :enclosures     (map obj->enclosure (seq (.getEnclosures e)))
+               :description    (or (if-let [d (.getDescription e)] (obj->content d))
+                                   (first (map obj->content (seq (.getContents e)))))
+               :author         (.getAuthor e)
+               :link           (.getLink e)
+               :published-date (.getPublishedDate e)
+               :title          (.getTitle e)
+               :updated-date   (.getUpdatedDate e)
+               :uri            (.getUri e)}))
+
+(defn- obj->feed [f]
+  (map->feed  {:authors        (map obj->person   (seq (.getAuthors f)))
+               :categories     (map obj->category (seq (.getCategories f)))
+               :contributors   (map obj->person   (seq (.getContributors f)))
+               :entries        (map obj->entry    (seq (.getEntries f)))
+               :entry-links    (map obj->link     (seq (.getLinks f)))
+               :image          (if-let [i (.getImage f)] (obj->image i))
+               :author         (.getAuthor f)
+               :copyright      (.getCopyright f)
+               :description    (.getDescription f)
+               :encoding       (.getEncoding f)
+               :feed-type      (.getFeedType f)
+               :language       (.getLanguage f)
+               :link           (.getLink f)
+               :published-date (.getPublishedDate f)
+               :title          (.getTitle f)
+               :uri            (.getUri f)}))
+
+(defn- parse-internal [xmlreader]
+  (let [feedinput (SyndFeedInput.)
+        syndfeed  (.build feedinput xmlreader)]
+    (obj->feed syndfeed)))
+
+(defn ->url [s]
+  (if (string? s) (URL. s) s))
+
+(defn parse-feed
+  ([feedsource]
+   (parse-internal (XmlReader. (->url feedsource))))
+  ([feedsource content-type]
+   (parse-internal (XmlReader. (->url feedsource) content-type)))
+  ([feedsource content-type lenient]
+   (parse-internal (XmlReader. (->url feedsource) content-type lenient)))
+  ([feedsource content-type lenient default-encoding]
+   (parse-internal (XmlReader. (->url feedsource) content-type lenient default-encoding))))
 
 (defn load-config []
   (try
@@ -37,26 +128,9 @@
           body (:body response)]
       (log/info "Feed fetched successfully from" url)
       (with-open [stream (ByteArrayInputStream. (.getBytes body "UTF-8"))]
-        (-> stream
-            xml/parse
-            zip/xml-zip)))
+        (parse-internal (XmlReader. stream))))
     (catch Exception e
       (log/error e "Error fetching feed from URL" url e)
-      nil)))
-
-(defn parse-feed [feed]
-  (try
-    (let [first-entry (zf/xml1-> feed :channel :item)]
-      (log/info "Parsing first feed entry")
-      (when first-entry
-        (let [title (safe-get first-entry #(zf/xml1-> % :title zf/text) "")
-              link (safe-get first-entry #(zf/xml1-> % :link zf/text) "")
-              description (safe-get first-entry #(zf/xml1-> % :description zf/text) "")]
-          {:title title
-           :link link
-           :description description})))
-    (catch Exception e
-      (log/error e "Error parsing feed entry" e)
       nil)))
 
 (defn fetch-feeds []
@@ -66,7 +140,7 @@
       (log/info "Feed URLs to fetch:" feed-urls)
       (let [feeds (map fetch-feed feed-urls)]
         (log/info "Feeds fetched, now parsing")
-        (let [parsed-feeds (filter some? (map parse-feed feeds))]
+        (let [parsed-feeds (filter some? (mapcat #(take 1 (:entries %)) feeds))]
           (log/info "Parsed feeds:" parsed-feeds)
           parsed-feeds)))
     (catch Exception e
